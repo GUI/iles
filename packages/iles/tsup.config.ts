92ec2798e4a1bdc3defcde59c8ad50440e0ba907
Maximo Mussini <maximomussini@gmail.com>
2021-12-07 16:17:08 -0300

    deps: update tsup

--- packages/iles/tsup.config.ts
+++ packages/iles/tsup.config.ts
@@ -1,22 +1,47 @@
-import type { Options } from 'tsup'
-export const tsup: Options = {
+import { defineConfig } from 'tsup'
+
+const baseConfig = {
   dts: true,
   target: 'node14',
   splitting: true,
   sourcemap: false,
-  format: ['cjs'],
-  outDir: 'dist/node',
-  external: [
-    '../../lib/modules',
-    '@vue/runtime-dom/dist/runtime-dom.esm-bundler.js',
-    'solid-js/web',
-    'preact-render-to-string',
-    '@islands/hydration/preact',
-    '@islands/hydration/dist/hydration.js',
-    '@islands/hydration/dist/vue.js',
-    '@islands/hydration/dist/vanilla.js',
-    '@islands/hydration/dist/solid.js',
-    '@islands/hydration/dist/preact.js',
-    '@islands/hydration/dist/svelte.js',
-  ],
 }
+
+export default defineConfig((options) => {
+  console.log({ options })
+  if (options.entryPoints[0] === 'src/client') {
+    console.log('here!')
+    return {
+      ...baseConfig,
+      format: ['esm'],
+      outDir: 'dist/client',
+      external: [
+        'components/App.vue',
+        'components/DebugPanel.vue',
+        'components/Head.vue',
+        'components/Island.vue',
+        'components/NotFound.vue',
+      ],
+    }
+  }
+  else {
+    return {
+      ...baseConfig,
+      format: ['cjs'],
+      outDir: 'dist/node',
+      external: [
+        '../../lib/modules',
+        '@vue/runtime-dom/dist/runtime-dom.esm-bundler.js',
+        'solid-js/web',
+        'preact-render-to-string',
+        '@islands/hydration/preact',
+        '@islands/hydration/dist/hydration.js',
+        '@islands/hydration/dist/vue.js',
+        '@islands/hydration/dist/vanilla.js',
+        '@islands/hydration/dist/solid.js',
+        '@islands/hydration/dist/preact.js',
+        '@islands/hydration/dist/svelte.js',
+      ],
+    }
+  }
+})
